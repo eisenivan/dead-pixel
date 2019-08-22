2504056df582e252e88b7375986627d40ff06354
Ivan Eisenberg <ivan.eisenberg@gmail.com>
2019-08-22 12:12:38 -0500

    Create dead-pixel.js

---
+++ dead-pixel.js
@@ -0,0 +1,14 @@
+;(function() {
+    'use strict'
+
+    const div = document.createElement("div")
+    div.style.width = '1px'
+    div.style.height = '1px'
+    div.style.background = 'black'
+    div.style.position = 'fixed'
+    div.style.left = '74.3%'
+    div.style.top = '17.3%'
+    div.style.zIndex = '99999'
+
+    document.body.appendChild(div)
+})()
